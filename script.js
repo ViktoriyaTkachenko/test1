e93aed697d574f78e2d487f8e439d9ff7852718b
VIktoriyaTkachenko <tkachenkov1994@gmail.com>
2019-10-03 21:32:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let $searchForm = $("#search-form");
+
+$searchForm.on("submit", function(event) {
+    event.preventDefault();
+
+    let query = $(this).find("[name='search-term']").val().replace(/\s/g, "+");
+
+    getBooks(query);
+});
+
+function getBooks(query) {
+    let url = 'https://www.googleapis.com/books/v1/volumes';
+    console.log("$", $);
+    $.ajax({
+        url,
+        method: "GET",
+        data: `q=${query}`
+    }).done((response) => {
+        console.log("response", response);
+    }).fail((error) => {
+        console.log("error", error);
+    })
+}
